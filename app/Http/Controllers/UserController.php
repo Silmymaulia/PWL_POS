97ee3986d09cdeb4852f9afd44b7491a52ec2c9b
Silmymaulia <silmy.smd@gmail.com>
2024-09-27 16:48:28 +0700

    Jobsheet 4 - Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,23 +10,29 @@ class UserController extends Controller
 {
     public function index() {
         //tambah data user dengan eloguent mode
-        $data = [
-            // 'level_id' => 2,
-            // 'username' => 'manager_dua',
-            // 'nama' => 'Manager 2',
-            // 'password' => Hash::make('12345')
+        // $data = [
+        //     // 'level_id' => 2,
+        //     // 'username' => 'manager_dua',
+        //     // 'nama' => 'Manager 2',
+        //     // 'password' => Hash::make('12345')
 
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 2',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-        // UserModel::insert($data); //tambahkan data ke tabel m_user
-        //UserModel::where('username', 'customer-1')->update($data); //update data user
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+        // // UserModel::insert($data); //tambahkan data ke tabel m_user
+        // //UserModel::where('username', 'customer-1')->update($data); //update data user
 
-        //coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // //coba akses model UserModel
+        // $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+        //praktikum 2
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
