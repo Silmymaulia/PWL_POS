ceb8730413d17da42f64f9247c737fa89c6dd35f
silmymaulia <silmy.smd@gmail.com>
2024-10-17 22:08:39 +0700

    Jobsheet 8 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -129,6 +129,8 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/barang/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
         Route::delete('/barang/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
         Route::delete('/barang/{id}', [BarangController::class, 'destroy']);
+        Route::get('/barang/import', [BarangController::class, 'import']); 
+        Route::post('/barang/import_ajax', [BarangController::class, 'import_ajax']);
     });
 
 });
