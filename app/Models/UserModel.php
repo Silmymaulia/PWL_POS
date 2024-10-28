57d2c1520194e63e4074440e05fd54281209a13e
silmymaulia <silmy.smd@gmail.com>
2024-10-28 09:57:46 +0700

    Jobsheet 10 - Praktikum 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // Implementasi class authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
 
