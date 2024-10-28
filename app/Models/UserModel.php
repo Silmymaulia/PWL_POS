1d233bdcd0cb4b8db58665435d6acaedb79e9332
silmymaulia <silmy.smd@gmail.com>
2024-10-28 09:20:51 +0700

    Jobsheet 10 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,6 +11,14 @@ class UserModel extends Authenticatable
 {
     use HasFactory;
 
+   public function getJWTIdentifier(){
+    return $this->getKey();
+   }
+
+   public function getJWTCustomClaims() {
+    return [];
+   }
+   
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     
